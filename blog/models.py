3f491e6390a15de3e2ab939d6d6242fad04ca520
Marso22 <marijonas@rocius.co.uk>
2025-08-02 23:10:01 +0100

    Add Post model with fields and migration for blog application

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
+class Post(models.Model):
+    CATEGORY_CHOICES = [
+        ('classic', 'Classic mind board games'),
+        ('strategy', 'Strategy mind board games'),
+        ('apps', 'Mind board games apps'),
+    ]
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    category = models.CharField(
+        max_length=20,
+        choices=CATEGORY_CHOICES,
+        default='classic',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    
+
+    def __str__(self):
+        return self.title
