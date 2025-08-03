7e19deab57b7334618a3c2318a9c6b64253dd28f
Marso22 <marijonas@rocius.co.uk>
2025-08-04 00:04:23 +0100

    add class Meta and def to class Comment

--- blog/models.py
+++ blog/models.py
@@ -28,7 +28,7 @@ class Post(models.Model):
     
     class Meta:
         ordering = ["-created_at"]
-    
+
     def __str__(self):
         return f"{self.title} | written by {self.author}"
 
@@ -41,3 +41,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
