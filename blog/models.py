153f35a53d6e76453137d79b7502f5d389a6b858
Marso22 <marijonas@rocius.co.uk>
2025-08-03 17:09:04 +0100

    Add excerpt field to Post model and comment out DEBUG setting in settings.py

--- blog/models.py
+++ blog/models.py
@@ -22,6 +22,7 @@ class Post(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
     
 
     def __str__(self):
